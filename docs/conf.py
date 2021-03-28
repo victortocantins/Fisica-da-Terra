c9cf9b461065be1aeb46d22fd5ea147128767657
victortocantins <victorcezar@ufpa.com>
2021-03-27 22:35:14 -0300

    Inicio do Projeto

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 # sys.path.insert(0, os.path.abspath('.'))
-from flask import request
+# from flask import request
 # sys.path.append(os.path.abspath('./_ext'))
 from sphinx_gallery.sorting import ExplicitOrder, FileNameSortKey
 
